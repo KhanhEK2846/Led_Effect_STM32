98414df9623440ba2e72a01a5676cbb954328136
Ngô Lê Tuyết Hoa <20521333@gm.uit.edu.vn>
2023-10-10 14:28:47 +0700

    Chỉnh sửa 10/10/2023
    - Sử dụng thêm timer 4 phục vụ cho việc xử lý cứ mỗi 200ms kể từ thời điểm nhấn nút giữ lâu hơn 500ms.
    - Hoàn thành chức năng UART phục vụ cho việc debug thông qua màn hình OLED (ESP32).
    * Report test KIT 10/10/2023:
    - Feature SW ổn.
    - KIT SSID ESP32 - S2 hư button1 nên không thể tương tác.
    * Việc cần làm:
    - Viết lib cho xử lý nút nhấn -> CLEAN CODE.
    - Vẫn là CLEAN CODE.
    - Viết báo cáo.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,9 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "string.h"
+#include "stdio.h"
+//---- Thu vien nut nhan - Author Hoa ----
+//#include "button.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -47,54 +49,55 @@ typedef enum
 
 /* Private variables ---------------------------------------------------------*/
 TIM_HandleTypeDef htim3;
+TIM_HandleTypeDef htim4;
 
 UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
 
-//khai bao bien luu trang thai
+//khai bao bien luu trang thai den
 Led_Effect_Status led_eff_state;
 
 //----- UART com debug variable -----
-uint8_t Tx_Buffer[50] = "Hello CE437"; //buffer truyen di
-uint16_t Tx_flag = 0;
-//----- UART com debug variable -----
-
-//---- led_time_cycle [min:max] variable ----
+uint8_t Tx_Buffer[100]; //buffer truyen di chua gia tri chu ky hien tai cua LED + che do hien tai cua mode led effect
+//---- led_time_cycle [min:max] variable + divine time (pwm)----
 uint16_t max_cycle_led_time = 2000, min_cycle_led_time = 0;
+uint16_t divine_time_val = 0; //bien dung de luu gia tri chia deu thoi gian hieu ung led (bam xung - pwm thu cong)
 //---- led_time_cycle [min:max] variable ----
 
 //---- Led counter effect variable -----
-uint8_t count_led_alt = 0, count_led_seq = 0;
+uint8_t count_led_alt = 0, count_led_seq = 0, count_led_blink = 0;
 //---- Led counter effect variable -----
 
-uint16_t divine_time_val = 0; //bien dung de luu gia tri chia deu thoi gian hieu ung led (bam xung - pwm thu cong)
-
 //---- button handle variable ----
-uint8_t button_current = 1; //trang thai hien tai cua nut bam, khoi tao la 1 de tranh dieu kien ban da
-uint8_t button_last = 1; //trang thai cuoi cua nut bam
-uint8_t button_filter = 1;
-uint8_t is_debouncing; //biet nhan biet nut dang rung do thay doi trang thai
-uint32_t time_debounce; //lay thoi gian thay doi trang thai
-uint32_t time_btn_press; //luu thoi gian ke tu luc nhan nut
-uint8_t is_press_timeout, longpress_timeout;
-uint32_t time_longpress_keep;
-//---- button handle variable ----
+uint8_t button1_current = 1; //trang thai hien tai cua nut bam, khoi tao la 1 de tranh dieu kien ban da
+uint8_t button1_last = 1; //trang thai cuoi cua nut bam
+uint8_t button1_filter = 1;
+
+uint8_t is_debouncing1; //biet nhan biet nut dang rung do thay doi trang thai
+uint32_t time_debounce1; //lay thoi gian thay doi trang thai
+uint32_t time_btn_press1; //luu thoi gian ke tu luc nhan nut
+uint8_t is_press_timeout1,is_long_press1;
+
+uint8_t button2_current = 1; //trang thai hien tai cua nut bam, khoi tao la 1 de tranh dieu kien ban da
+uint8_t button2_last = 1; //trang thai cuoi cua nut bam
+uint8_t button2_filter = 1;
+
+uint8_t is_debouncing2; //biet nhan biet nut dang rung do thay doi trang thai
+uint32_t time_debounce2; //lay thoi gian thay doi trang thai
+uint32_t time_btn_press2; //luu thoi gian ke tu luc nhan nut
+uint8_t is_press_timeout2, is_long_press2;
 
 
-uint16_t led_timer_cnt = 0;
+uint8_t cycle_change_mode = 0; //mode = 0: dec mode = 1 inc
 
+//----timer count ----
+uint16_t is_press_time_count = 0; //button press timer count (for longpress support purpose)
+uint16_t led_timer_cnt = 0;		//timer count for led trasition time
+
+//--------------------
 //test_var
-int count = 0;
-int temp1,temp2,temp3;
 int test;
-
-//test = 2 press button call back
-//test = 1 short press
-//test = 3 long pressing
-//test = 4 release button call back
-//test =  5 test long press if else
-
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -102,90 +105,185 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART1_UART_Init(void);
 static void MX_TIM3_Init(void);
+static void MX_TIM4_Init(void);
 /* USER CODE BEGIN PFP */
 void led_blink_origin(void); //effect 1
 void led_blink_sequence(void); //effect 2
 void led_blink_alternate(void); //effect 3
-void button_hanlde(void);
+
+void button1_hanlde(void);
+void button2_handle(void);
+
 void button_pressing_callback(void);
 void button_release_callback(void);
-void button_shortpressing_callback_500ms(void); //button1 < 500ms handle fnct
-void button_longpressing_callback_500ms(void); ////button1 > 500ms handle fnct
+
+void button1_shortpressing_callback_500ms(void); //button1 < 500ms handle fnct
+void button1_longpressing_callback_500ms(void); ////button1 > 500ms handle fnct
+
+void button2_shortpressing_callback_500ms(void); //button2 < 500ms handle fnct
+void button2_longpressing_callback_500ms(void); ////button2 > 500ms handle fnct
+
+void time_cycle_inc_callback(void);
+void time_cycle_dec_callback(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-void button_handle(void)
+void button1_handle(void)
 {
 	//-------- Xu ly loc nhieu ------
-	//khong dung timer interrupt phuc vu loc nhieu
+	//dung timer systick phuc vu loc nhieu
 	//giai thuat kieu:
 	//1. phat hien thay doi trang thai
 	//2. cu co nhieu, reset time_debounce
-	uint8_t sta = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0);
+	uint8_t sta1 = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0); //BTN1
+	//KIT truong button1 hardware hong - SSID: ESP32_S2
 	//cu co nhieu gan lai button filter
-	if(sta != button_filter) //trang thai doc duoc, khac voi trang thai truoc (button_filter)
+	if(sta1 != button1_filter) //trang thai doc duoc, khac voi trang thai truoc (button_filter)
 	{
 		//co su thay doi trang thai thi se vao day gan lien tuc vao button filter
-		button_filter = sta;
-		is_debouncing = 1;
-		time_debounce = HAL_GetTick(); //moi lan co xung nhieu time debounce duoc cap nhat lai
+		button1_filter = sta1;
+		is_debouncing1 = 1;
+		time_debounce1 = HAL_GetTick(); //moi lan co xung nhieu time debounce duoc cap nhat lai
 	}
-	//-------- Xu ly loc nhieu ------
 	//-------- Xac lap tin hieu ------
-	if(is_debouncing == 1 && (HAL_GetTick()-time_debounce >= 15)) //check nhieu
+	if(is_debouncing1 == 1 && (HAL_GetTick()-time_debounce1 >= 15)) //check nhieu (standard vong check nhieu = 15ms) - callib
 	{
-		button_current = button_filter;
-		is_debouncing = 0;
+		button1_current = button1_filter;
+		is_debouncing1 = 0;
 	}
-	//-------- Xac lap tin hieu ------
 	//-------- Xu ly nut nhan --------
-	if(button_current != button_last)
+	if(button1_current != button1_last)
 	{
-		if(button_current == 0) //truong hop dang nhan nut
+		if(button1_current == 0) //truong hop dang nhan nut
 		{
-			//nhan xuong lam gi do thi nen viet ham callback - lap trinh nen quen callback - Nhan nut tao ra su kien
-			button_pressing_callback();
-			is_press_timeout = 1;
-			time_btn_press = HAL_GetTick();
+			//button_pressing_callback();
+			is_press_timeout1 = 1;
+			time_btn_press1 = HAL_GetTick();
 		} else { //nha nut
-			//nha nut se lam su kien gi day - viet dang callback
 			//tuc la khoang thoi gian ke tu luc nhan cho den luc nha ra < 500ms
-			temp1 = HAL_GetTick();
-			if(HAL_GetTick() - time_btn_press < 500)
+			if(HAL_GetTick() - time_btn_press1 < 500)
 			{
-				test = 1;
-				button_shortpressing_callback_500ms();
-				is_press_timeout = 0;
+				button1_shortpressing_callback_500ms();
+				is_press_timeout1 = 0;
 			}
+			is_long_press1 = 0;
 			//button_release_callback();
 		}
-		button_last = button_current; //muc dich neu trang thai nut bam sau khi xac lap khac trang thai truoc thi xu ly callback
+		button1_last = button1_current; //muc dich neu trang thai nut bam sau khi xac lap khac trang thai truoc thi xu ly callback
 	}
-	temp2 = HAL_GetTick();
-	if(is_press_timeout == 1 && (HAL_GetTick() - time_btn_press > 500))
+	if(is_press_timeout1 == 1 && (HAL_GetTick() - time_btn_press1 >= 500))
 	{
-			//is_press_timeout = 0;
-			test = 5;
-			longpress_timeout = 1;
-			time_longpress_keep = HAL_GetTick();
-			button_longpressing_callback_500ms();
+		button1_longpressing_callback_500ms();
+	}
+}
+
+void button2_handle(void)
+{
+	//-------- Xu ly loc nhieu ------
+	//dung timer systick phuc vu loc nhieu
+	//giai thuat kieu:
+	//1. phat hien thay doi trang thai
+	//2. cu co nhieu, reset time_debounce
+	uint8_t sta2 = HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1); //BTN2
+	//cu co nhieu gan lai button filter
+	if(sta2 != button2_filter) //trang thai doc duoc, khac voi trang thai truoc (button_filter)
+	{
+		//co su thay doi trang thai thi se vao day gan lien tuc vao button filter
+		button2_filter = sta2;
+		is_debouncing2 = 1;
+		time_debounce2 = HAL_GetTick(); //moi lan co xung nhieu time debounce duoc cap nhat lai
+	}
+	//-------- Xac lap tin hieu ------
+	if(is_debouncing2 == 1 && (HAL_GetTick()-time_debounce2 >= 15)) //check nhieu (standard vong check nhieu = 15ms) - callib
+	{
+		button2_current = button2_filter;
+		is_debouncing2 = 0;
 	}
 	//-------- Xu ly nut nhan --------
+	if(button2_current != button2_last)
+	{
+		if(button2_current == 0) //truong hop dang nhan nut
+		{
+			//button_pressing_callback();
+			is_press_timeout2 = 1;
+			time_btn_press2 = HAL_GetTick();
+		} else { //nha nut
+			//tuc la khoang thoi gian ke tu luc nhan cho den luc nha ra < 500ms
+			if(HAL_GetTick() - time_btn_press2 < 500)
+			{
+				button2_shortpressing_callback_500ms();
+				is_press_timeout2 = 0;
+			}
+			is_long_press2 = 0;
+			//button_release_callback();
+		}
+		button2_last = button2_current; //muc dich neu trang thai nut bam sau khi xac lap khac trang thai truoc thi xu ly callback
+	}
+	if(is_press_timeout2 == 1 && (HAL_GetTick() - time_btn_press2 >= 500))
+	{
+		button2_longpressing_callback_500ms();
+	}
 }
 
 void button_pressing_callback(void)
 {
-	//test giam chu ky led timer
-	//chua tinh truong hop nhan nhanh, nhan lau
-	//thu nhan doi hieu ung
-	//count++;
-	//tam thoi do nothing
 	test = 2;
+	//time_cycle_dec_callback();
 }
 
-void button_shortpressing_callback_500ms(void)
+void button_release_callback(void)
+{
+	test = 4;
+}
+
+void button1_shortpressing_callback_500ms(void)
+{
+	time_cycle_dec_callback();
+}
+
+void button2_shortpressing_callback_500ms(void)
+{
+	switch(led_eff_state)
+	{
+		case LED_BLINK_ORIGIN:
+				led_eff_state = LED_BLINK_SEQ;
+		break;
+		case LED_BLINK_SEQ:
+			led_eff_state = LED_BLINK_ALTER;
+		break;
+		case LED_BLINK_ALTER:
+			led_eff_state = LED_BLINK_ORIGIN;
+		break;
+	}
+}
+
+
+void button1_longpressing_callback_500ms(void)
+{
+	cycle_change_mode = 0;
+	is_long_press1 = 1;
+	is_press_timeout1 = 0;
+}
+
+void button2_longpressing_callback_500ms(void)
+{
+	cycle_change_mode = 1;
+	is_long_press2 = 1;
+	is_press_timeout2 = 0;
+}
+
+void time_cycle_inc_callback(void)
+{
+	if(max_cycle_led_time == 2000)
+	{
+		max_cycle_led_time = 0;
+	}
+	max_cycle_led_time += 100;
+}
+
+void time_cycle_dec_callback(void)
 {
 	if(max_cycle_led_time == 0)
 	{
@@ -194,38 +292,19 @@ void button_shortpressing_callback_500ms(void)
 	max_cycle_led_time -= 100;
 }
 
-
-void button_longpressing_callback_500ms(void)
-{
-	//sau moi khoang thoi gian 200ms duoc nhan giu
-	//giam chu ky led di dan tuc la "nut duoc nhan va giu hoai luon cu moi 200ms thi -100ms max-cycle
-	temp3 = HAL_GetTick();
-	if(longpress_timeout && (HAL_GetTick() - time_longpress_keep == 200))
-	{
-		test = 3;
-		if(max_cycle_led_time == 0)
-		{
-			max_cycle_led_time = 2000;
-		}
-		max_cycle_led_time -= 100;
-		//is_press_timeout = 0;
-	}
-	longpress_timeout = 0;
-}
-
-void button_release_callback(void)
-{
-	//do nothinng
-	//co the lam gi do nhung chua biet la nen lam gi
-	test = 4;
-}
-
-
 void led_blink_origin(void)
 {
-	HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
-	HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_4);
-	HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_5);
+	if(count_led_blink == 0){
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
+		count_led_blink = 1;
+	} else if (count_led_blink == 1){
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+		count_led_blink = 0;
+	}
 }
 
 void led_blink_sequence(void)
@@ -235,19 +314,19 @@ void led_blink_sequence(void)
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_SET);
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
-		count_led_seq += 1;
+		count_led_seq = 1;
 	} else if(count_led_seq == 1)
 	{
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
-		count_led_seq += 1;
+		count_led_seq = 2;
 	} else if (count_led_seq == 2)
 	{
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
-		count_led_seq += 1;
+		count_led_seq = 3;
 	} else if (count_led_seq == 3)
 	{
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
@@ -264,7 +343,7 @@ void led_blink_alternate(void)
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3,GPIO_PIN_SET); //off
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4,GPIO_PIN_RESET); //on
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5,GPIO_PIN_SET); //off
-		count_led_alt += 1;
+		count_led_alt = 1;
 	} else if (count_led_alt == 1)
 	{
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3,GPIO_PIN_RESET); //on
@@ -306,10 +385,11 @@ int main(void)
   MX_GPIO_Init();
   MX_USART1_UART_Init();
   MX_TIM3_Init();
-  HAL_TIM_Base_Start_IT(&htim3);
+  MX_TIM4_Init();
   /* USER CODE BEGIN 2 */
-  //uint8_t btn_state = 0;
-  //uint8_t btn2_state = 0;
+  HAL_TIM_Base_Start_IT(&htim3);
+  HAL_TIM_Base_Start_IT(&htim4);
+
 
   /* USER CODE END 2 */
 
@@ -317,14 +397,16 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	//----- Phan code test debug UART gui den man hinh OLED ----
+	//clear buffer string before write below:
+	//-- code
+	sprintf((char*)Tx_Buffer,"\nCYCLE:%u\nMODE:%u",max_cycle_led_time,led_eff_state);
+	HAL_UART_Transmit(&huart1,Tx_Buffer,sizeof(Tx_Buffer), 10);
 	//----- Phan code test debug UART gui den man hinh OLED -----
-//	if(Tx_flag)
-//	{
-//		 HAL_UART_Transmit(&huart1, Tx_Buffer,strlen(Tx_Buffer), 100);
-//	}
-//	HAL_Delay(300); // cu moi 300 ms gui 1 lan - thu 2 len truong test
-	//----- Phan code test debug UART gui den man hinh OLED -----
-	 button_handle();
+	button1_handle();
+	button2_handle();
+
+	HAL_Delay(100);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -417,6 +499,51 @@ static void MX_TIM3_Init(void)
 }
 
 /**
+  * @brief TIM4 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM4_Init(void)
+{
+
+  /* USER CODE BEGIN TIM4_Init 0 */
+
+  /* USER CODE END TIM4_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM4_Init 1 */
+
+  /* USER CODE END TIM4_Init 1 */
+  htim4.Instance = TIM4;
+  htim4.Init.Prescaler = 3599;
+  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim4.Init.Period = led_TIM_counter;
+  htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM4_Init 2 */
+
+  /* USER CODE END TIM4_Init 2 */
+
+}
+
+/**
   * @brief USART1 Initialization Function
   * @param None
   * @retval None
@@ -484,7 +611,6 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) //!WARNING: Don't change
 {
-
 	if(htim == &htim3) //behavior led function timer
 	{
 		if(led_timer_cnt > max_cycle_led_time)
@@ -530,6 +656,19 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) //!WARNING: Don't ch
 			} else if(max_cycle_led_time == 0)
 			{
 				max_cycle_led_time = 2000; //neu chu ky bi nut bam tuong tac giam ve 0 thi reset lai thoi gian chu ky
+			}
+		}
+	}
+	if(htim == &htim4 && (is_long_press1==1 || is_long_press2==1))
+	{
+		//is_press_time_count++;
+		if(is_press_time_count++ == 200)
+		{
+			is_press_time_count = 0;
+			if(cycle_change_mode == 0) {
+				time_cycle_dec_callback();
+			} else if(cycle_change_mode == 1){
+				time_cycle_inc_callback();
 			}
 		}
 	}
